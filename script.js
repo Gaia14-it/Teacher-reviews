039fbe0ea00ad4a6511c33f561f84f08d1cfaff1
gaia14-it <gaiamaria14n@gmail.com>
2025-03-17 11:44:45 -0400

    went on with js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let form = document.querySelector(".fill")
 async function request(method, endpoint, data = null) {
     let userId = btoa("natale:605292")
     const headers = {
-        "content-Type": "application/json"
-        "Authorization": `Basic ${userId}`
+        "content-Type": "application/json",
+        "Authorization": `Basic 1234`
     };
     
     let options = {
@@ -16,7 +16,7 @@ async function request(method, endpoint, data = null) {
         options["body"] = JSON.stringify(data);
     }
 
-    // let request = await fetch()
+    let request = await fetch(`http://bucks.local/${endpoint}`, options)
 
     if(request.ok) {
         let result = await request.json()
@@ -25,7 +25,7 @@ async function request(method, endpoint, data = null) {
 }
 
 window.addEventListener("DOMContentLoaded", async function() {
-    let result = await request("get", "todo", null);
+    let result = await request("get", "course-todo", null);
     render(result.data) 
 })
 
@@ -33,3 +33,23 @@ form.addEventListener("submit", (event) => {
     event.preventDefault();
     postTodo(event.target);
 }) 
+
+async function postTodo(form) {
+    let formData = Object.fromEntries(new FormData(form));
+    let result = await request("post", "course-todo", formData);
+    render(result.data)
+    form.reset()
+}
+
+let tasksContainer = document.querySelector(".tasks")
+
+function render(todoList) {
+    tasksContainer.innerHTML = todoList.map(course-todo => {
+        return`
+        <ul>
+        <li> ${todo.title}</li>
+        <button data-todoid = "${course-todo.id}" onclick = "deleteTodo(this)"
+        `
+    })
+}
+
